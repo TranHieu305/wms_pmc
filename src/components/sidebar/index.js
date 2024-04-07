ed45bd77611234d68be29a68b2c96e67faf31df1
TranHieu305 <trunghieu2000.tdn@gmail.com>
2024-04-08 03:21:05 +0700

    create 'useFetch' hook

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,17 +1,67 @@
-import { Boxes } from "lucide-react";
-import { Menu, MenuItem } from "../ui/menu";
+import { Layout, Menu, Space, Table, Typography } from "antd";
+import { PauseOutlined } from "@ant-design/icons";
+// import Header from "../header";
+
+import "./index.css";
+import { Link } from "react-router-dom";
+
+const { Sider } = Layout;
 
 export default function Sidebar() {
 	return (
-		<>
-			{/* header */}
+		<Layout>
+			<Sider className="sidebar">
+				Sidebar
+				<Menu theme="dark" mode="inline">
+					<Menu.Item key="home" icon={<PauseOutlined />}>
+						<Link to="home">Home</Link>
+					</Menu.Item>
+					<Menu.Item key="home2" icon={<PauseOutlined />}>
+						Home
+					</Menu.Item>
+					<Menu.Item key="home3" icon={<PauseOutlined />}>
+						Home
+					</Menu.Item>
+					<Menu.SubMenu key="subtasks" icon={<PauseOutlined />} title="Submenu">
+						<Menu.Item key="home4" icon={<PauseOutlined />}>
+							Home
+						</Menu.Item>
+						<Menu.Item key="home5" icon={<PauseOutlined />}>
+							Home
+						</Menu.Item>
+					</Menu.SubMenu>
+				</Menu>
+			</Sider>
+			{/* <Header title="Header" subtitle="Sub header" /> */}
 
-			<p>Hello World</p>
-			<Menu>
-				<MenuItem icon={<Boxes />} title="Home" path="home" />
-				<MenuItem icon={<Boxes />} title="Home" />
-				<MenuItem icon={<Boxes />} title="Home" />
-			</Menu>
-		</>
+			{/* Table test  */}
+			<Space size={20}>
+				<Typography.Title level={4}>Warehouse</Typography.Title>
+				<Table
+					columns={[
+						{
+							title: "title",
+							dataIndex: "title",
+						},
+						{
+							title: "price",
+							dataIndex: "price",
+						},
+						{
+							title: "price",
+							dataIndex: "price",
+						},
+						{
+							title: "title",
+							dataIndex: "title",
+						},
+						{
+							title: "title",
+							dataIndex: "title",
+						},
+					]}
+				></Table>
+			</Space>
+		</Layout>
 	);
 }
