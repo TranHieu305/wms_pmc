92e394d181ecbf101d84d1f0fc5d3f550e88677c
TranHieu305 <trunghieu2000.tdn@gmail.com>
2024-04-15 07:55:51 +0700

    Update Sidemenu UI

--- src/components/layout/SideMenu.js
+++ src/components/layout/SideMenu.js
@@ -1,9 +1,10 @@
-import { Button, Menu } from "antd";
+import { Avatar, Button, Menu } from "antd";
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { LuWarehouse } from "react-icons/lu";
-import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineMenu, AiOutlineSetting } from "react-icons/ai";
 import { TeamOutlined } from "@ant-design/icons";
+import { BiLogOutCircle } from "react-icons/bi";
 import "../../styles/layout.css";
 
 function SideMenu() {
@@ -18,20 +19,49 @@ function SideMenu() {
 
 	return (
 		<div className="sidemenu">
-			{/* <div className="sidemenu-header">
-				<Button icon={<AiOutlineMenu />}></Button>
-			</div> */}
-			<Menu
-				className="sidemenu-vertical"
-				mode="vertical"
-				onClick={(item) => navigate(item.key)}
-				selectedKeys={[selectedKeys]}
-				items={[
-					{ label: "Warehouse", key: "/warehouses", icon: <LuWarehouse /> },
-					// { label: "Supplier", key: "/suppliers", icon: <TeamOutlined /> },
-					{ label: "Customer", key: "/customers", icon: <TeamOutlined /> },
-				]}
-			></Menu>
+			<div className="sidemenu-header">
+				<Button icon={<AiOutlineMenu />} type="dash"></Button>
+			</div>
+
+			{/* User infor */}
+			<div className="sidemenu-user">
+				<div className="sidemenu-user-container">
+					<Avatar
+						size={60}
+						className="avatar"
+						style={{ backgroundColor: "#fde3cf", color: "#f56a00" }}
+					>
+						U
+					</Avatar>
+					<span>User</span>
+				</div>
+			</div>
+
+			{/* Side menu */}
+			<div className="sidemenu-main">
+				<Menu
+					className="sidemenu-vertical"
+					mode="vertical"
+					onClick={(item) => navigate(item.key)}
+					selectedKeys={[selectedKeys]}
+					items={[
+						{ label: "Warehouse", key: "/warehouses", icon: <LuWarehouse /> },
+						// { label: "Supplier", key: "/suppliers", icon: <TeamOutlined /> },
+						{ label: "Customer", key: "/customers", icon: <TeamOutlined /> },
+					]}
+				></Menu>
+			</div>
+
+			{/* Logout and setting */}
+
+			<div className="sidemenu-footer">
+				<Button shape="round" icon={<BiLogOutCircle />}>
+					Logout
+				</Button>
+				<Button shape="round" icon={<AiOutlineSetting />}>
+					Setting
+				</Button>
+			</div>
 		</div>
 	);
 }
